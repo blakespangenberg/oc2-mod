8caf8d9888dae0162583bd39a1a4a496c67e242b
Blake Spangenberg <blake.spangenberg1@gmail.com>
2020-02-01 20:22:38 -0800

    Added library/references

--- oc2-mod/oc2-mod/Class1.cs
+++ oc2-mod/oc2-mod/Class1.cs
@@ -1,11 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
 
-namespace oc2_mod
+using UnityEngine;
+using Harmony;
+using UnityModManagerNet;
+
+namespace DifficultyOptions
 {
-    public class Class1
+    static class Main
     {
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            mod = modEntry;
+            mod.OnUpdate = OnUpdate;
+            return true;
+        }
+
+        static void OnUpdate(UnityModManager.ModEntry modEntry, float dt)
+        {
+            if(Input.GetKeyDown(KeyCode.F1))
+            {
+                FileLog.Log("Restored health");
+                HealthAndDamageNew component = GameObject.FindWithTag("Ship").GetComponent<HealthAndDamageNew>();
+                component.health = component.initialHealth;
+            }
+        }
+    }
+
+    [HarmonyPatch] // Class
+    [HarmonyPatch("processCollision")]    // Method
+    static class SkipMacGuffinHealth
+    {
+        static bool Prefix(GameObject other)
+        {
+            if(other.CompareTag("MacGuffin"))
+            {
+                GameObject.FindWithTag("CampaignLevel").GetComponent<CampaignLevelController>().MacGuffinAcquired();
+                other.GetComponent<PowerupController>().Poof();
+                return false; // don't run the original
+            }
+
+            return true; // run the original
+        }
     }
 }
