35ef2fb43f26ca70b5720b82d334ebcb8b4f9d06
Blake Spangenberg <blake.spangenberg1@gmail.com>
2020-04-28 14:09:48 -0700

    downgraded to .net 3.5 framework

--- OC2Mod/Main.cs
+++ OC2Mod/Main.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 using UnityEngine;
 using UnityModManagerNet;
+using Harmony;
+
+/*
+ * These are functions that the mod manager will call if implemented
+ * 
+OnGUI         - Called to draw UI.
+OnSaveGUI     - Called while saving.
+OnUpdate      - Called by MonoBehaviour.Update.
+OnLateUpdate  - Called by MonoBehaviour.LateUpdate.
+OnFixedUpdate - Called by MonoBehaviour.FixedUpdate.
+OnShowGUI     - Called when opening mod GUI.
+OnHideGUI     - Called when closing mod GUI.
+*/
 
 namespace OC2Mod
 {
@@ -13,20 +26,12 @@ namespace OC2Mod
     {
         static bool Load(UnityModManager.ModEntry modEntry)
         {
+            // var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            // harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            modEntry.OnUpdate = OnUpdate;
             return true;
         }
-
-        /*
-         * These are functions that the mod manager will call if implemented
-         * 
-        OnGUI         - Called to draw UI.
-        OnSaveGUI     - Called while saving.
-        OnUpdate      - Called by MonoBehaviour.Update.
-        OnLateUpdate  - Called by MonoBehaviour.LateUpdate.
-        OnFixedUpdate - Called by MonoBehaviour.FixedUpdate.
-        OnShowGUI     - Called when opening mod GUI.
-        OnHideGUI     - Called when closing mod GUI.
-        */
 
         static void OnUpdate(UnityModManager.ModEntry modEntry, float dt)
         {
@@ -35,5 +40,20 @@ namespace OC2Mod
                 modEntry.Logger.Log("F1 pressed");
             }
         }
+
+        /*
+        [HarmonyPatch(typeof(SurfaceMovable))] // Class
+        [HarmonyPatch("GetVelocity")]          // Method
+        static class DoubleSpeed
+        {
+            static bool Prefix(Vector3 __result)
+            {
+                __result = new Vector3();
+                __result.Set(10,10,10);
+
+                return false;
+            }
+        }
+        */
     }
 }
