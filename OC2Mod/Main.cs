622a3faa705ff6b9e7ce1ae63543e6ce0ba261d7
Blake Spangenberg <blake.spangenberg1@gmail.com>
2020-04-30 18:27:20 -0700

    Even more horde options exposed

--- OC2Mod/Main.cs
+++ OC2Mod/Main.cs
@@ -26,8 +26,8 @@ namespace OC2Mod
     public class Main
     {
         static UnityModManager.ModEntry mod;
-        const float HORDE_STAGGER_TIME_MULTIPLIER = 1.0f;
-        const float DISH_WASH_TIME_MULTIPLIER = 1.0f;
+        const float DISH_WASH_TIME_MULTIPLIER    = 1.0f; // larger means longer wash time
+        const float HORDE_SPAWN_SPEED_MULTIPLIER = 1.0f; // larger means less stagger
 
         static bool Load(UnityModManager.ModEntry modEntry)
         {
@@ -90,7 +90,7 @@ namespace OC2Mod
             {
                 mod.Logger.Log("FastHorde.PostFix()");
 
-                double waveTimeScaled = waveTime* HORDE_STAGGER_TIME_MULTIPLIER;
+                double waveTimeScaled = waveTime* HORDE_SPAWN_SPEED_MULTIPLIER;
                 for (int i = 0; i < spawns.Count; i++)
                 {
                     if (spawns[i].CanSpawn(waveTimeScaled))
@@ -104,12 +104,12 @@ namespace OC2Mod
             }
         }
 
-        const float HORDE_PLATE_RETURN_TIME_MULTIPLIER       = 1.0f;
-        const float HORDE_TARGET_HEALTH_MULTIPLIER           = 1.0f;
-        const float HORDE_TARGET_REPAIR_TIME_MULTIPLIER      = 1.0f;
-        const float HORDE_TARGET_REPAIR_THRESHOLD_MULTIPLIER = 1.0f;
-        const float HORDE_TARGET_REPAIR_COST_MULTIPLIER      = 1.0f;
-        const float HORDE_HEALTH_MULTIPLIER                  = 1.0f;
+        const float HORDE_PLATE_RETURN_TIME_MULTIPLIER       = 1.0f; // untested
+        const float HORDE_TARGET_HEALTH_MULTIPLIER           = 1.0f; // untested
+        const float HORDE_TARGET_REPAIR_SPEED_MULTIPLIER     = 1.0f; // untested
+        const float HORDE_TARGET_REPAIR_THRESHOLD_MULTIPLIER = 1.0f; // untested
+        const float HORDE_TARGET_REPAIR_COST_MULTIPLIER      = 1.0f; // untested
+        const float HORDE_HEALTH_MULTIPLIER                  = 1.0f; // untested
         
         // const float HORDE_ENEMY_COUNT_MULTIPLIER = 1.0f;
 
@@ -126,7 +126,7 @@ namespace OC2Mod
 
                 result.m_plateReturnTime        =        10f * HORDE_PLATE_RETURN_TIME_MULTIPLIER;
                 result.m_targetHealth           = (int) (100 * HORDE_TARGET_HEALTH_MULTIPLIER);
-                result.m_targetRepairSpeed      =       0.5f * HORDE_TARGET_REPAIR_TIME_MULTIPLIER;
+                result.m_targetRepairSpeed      =       0.5f * HORDE_TARGET_REPAIR_SPEED_MULTIPLIER;
                 result.m_targetRepairThreshold  = (int) (10f * HORDE_TARGET_REPAIR_THRESHOLD_MULTIPLIER);
                 result.m_targetRepairCostMax    = (int) (200 * HORDE_TARGET_REPAIR_COST_MULTIPLIER);
                 result.m_health                 = (int) (100 * HORDE_HEALTH_MULTIPLIER);
@@ -142,6 +142,29 @@ namespace OC2Mod
                     }
                 }
                 */
+            }
+        }
+
+        const float HORDE_ENEMY_KITCH_ATTACK_SPEED_MULTIPLIER  = 1.0f; // larger means slower attack rate
+        const float HORDE_ENEMY_TARGET_DAMAGE_MULTIPLIER       = 1.0f; // larger means more damage
+        const float HORDE_ENEMY_TARGET_ATTACK_SPEED_MULTIPLIER = 1.0f; // larger means slower attack rate
+        const float HORDE_ENEMY_KITCH_DAMAGE_MULTIPLIER        = 1.0f; // larger means more damage
+        const int   HORDE_ENEMY_RECIPIE_COUNT                  = 1; // doesn't work
+        const float HORDE_ENEMY_MOVEMENT_SPEED_MULTIPLIER      = 1.0f; // larger means move to window faster
+        [HarmonyPatch(typeof(GameModes.Horde.ServerHordeEnemy))]     // Class
+        [HarmonyPatch("OnUpdateState")]                              // MethodPostfix
+        static class HordeEnemyPath
+        {
+            static bool Prefix(ref GameModes.Horde.HordeEnemy ___m_enemy)
+            {
+                ___m_enemy.m_attackKitchenFrequencySeconds = 5f * HORDE_ENEMY_KITCH_ATTACK_SPEED_MULTIPLIER;
+                ___m_enemy.m_targetDamage = (int) (25 * HORDE_ENEMY_TARGET_DAMAGE_MULTIPLIER);
+                ___m_enemy.m_attackTargetFrequencySeconds = 5f * HORDE_ENEMY_TARGET_ATTACK_SPEED_MULTIPLIER;
+                ___m_enemy.m_kitchenDamage = (int) (10 * HORDE_ENEMY_KITCH_DAMAGE_MULTIPLIER);
+                ___m_enemy.m_recipeCount   = HORDE_ENEMY_RECIPIE_COUNT;
+                ___m_enemy.m_movementSpeed = 1f * HORDE_ENEMY_MOVEMENT_SPEED_MULTIPLIER;
+
+                return true;
             }
         }
     }
